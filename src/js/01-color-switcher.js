06490b3f9b3168caa241e78c5317d247f0412070
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-10-09 13:11:23 +0300

    dz092

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const changeColor = document.querySelector('body');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+btnStop.setAttribute('disable', '');
+
+btnStart.addEventListener('click', element => {
+  element.target.setAttribute('disable', true);
+  btnStop.removeAttribute('disable');
+
+  intervalId = setInterval(() => {
+    changeColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', element => {
+  element.target.setAttribute('disable', true);
+  btnStart.removeAttribute('disable');
+  clearInterval(intervalId);
+});
