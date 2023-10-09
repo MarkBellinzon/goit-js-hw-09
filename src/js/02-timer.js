4628db470e17c76db2395bf3fb9439e160e9a7d4
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-10-09 19:13:57 +0300

    dz093

---
+++ src/js/02-timer.js
@@ -0,0 +1,39 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const selectors = {
+  input: document.querySelector('#datetime-picker'),
+  start: document.querySelector('button[data-start'),
+  days: document.querySelector('#datetime-picker'),
+  hours: document.querySelector('#datetime-picker'),
+  minuts: document.querySelector('#datetime-picker'),
+  seconds: document.querySelector('#datetime-picker'),
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options);
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,28 @@
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+  e.preventDefualt();
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  createPromise(2, 1500)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
