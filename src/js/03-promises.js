01ef6da184e137b364d377478ae9125e0865358c
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-10-10 13:42:24 +0300

    dz094

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,11 @@
 import Notiflix from 'notiflix';
 
 const formRef = document.querySelector('.form');
-formRef.addEventListener('submit', onSubmit);
-function onSubmit(e) {
-  e.preventDefualt();
-}
 
 function createPromise(position, delay) {
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
+
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
@@ -17,12 +14,43 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
+}
 
-  createPromise(2, 1500)
-    .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
+// promise
+//   .then(({ position, delay }) => {
+//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+formRef.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const { delay, step, amount } = evt.currentTarget.elements;
+  for (let i = 0; i < amount.value; i++) {
+    let position = i + 1;
+    const delays = Number(delay.value) + step.value * i;
+
+    // console.log('position');
+
+    createPromise(position, delays)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+  if (delay.value > 0 || step.value > 0 || amount.value > 0) {
+  } else {
+    return;
+  }
+
+  evt.currentTarget.reset();
 }
