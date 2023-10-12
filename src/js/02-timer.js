8852fbaf80b52dd0e6a196d1cf3f2284864a81e1
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-10-12 11:36:22 +0300

    dz096

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -24,9 +24,8 @@ flatpickr(date, {
       Notiflix.Notify.failure('Please choose a date in the future');
       btn.disabled = true;
     } else {
-      btn.disabled = false;
-
       Notiflix.Notify.success('Lets go?');
+      btn.disabled = false;
     }
   },
 });
@@ -56,19 +55,14 @@ function onBtnStartClick() {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
